1f7821c48d58293ad1b4863353c4bfc7c31f9a22
Florian Bauer <florian.bauer@adago.de>
2025-06-03 09:21:26 +0200

    update main for evaluation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Main module to run GRPO on CartPole-v1 environment."""
+from datetime import datetime
 import warnings
 
 import numpy as np
@@ -11,63 +12,87 @@ from sb3_contrib.grpo.buffers import StepwiseGroupBuffer
 
 warnings.filterwarnings("error", category=RuntimeWarning)  # DEBUG line for temporarily converting warnings to errors
 
-TRAINING_TIMESTEPS = 500_000
-EVAL_TIMESTEPS = 100
-USE_BASELINE = False
+TRAINING_TIMESTEPS = 100_000
+RUN_TIMESTEPS = 100
+
+RUN_GRPO = False
+RUN_PPO = False
+
+N_EVAL_EPISODES = 5
+EVAL_FREQ = 1000
+GRPO_EVAL_PATH = "./eval_logs/grpo/"
+PPO_EVAL_PATH = "./eval_logs/ppo/"
 
 grpo_vec_env = make_vec_env("CartPole-v1", n_envs=1)
 ppo_vec_env = make_vec_env("CartPole-v1", n_envs=1)
-eval_env = make_vec_env("CartPole-v1", n_envs=1)
+grpo_eval_env = make_vec_env("CartPole-v1", n_envs=1)
+ppo_eval_env = make_vec_env("CartPole-v1", n_envs=1)
 
-eval_callback = EvalCallback(
-    eval_env,
-    log_path="./eval_logs/",
-    eval_freq=10000,
-    n_eval_episodes=5,
+grpo_eval_callback = EvalCallback(
+    grpo_eval_env,
+    log_path=GRPO_EVAL_PATH,
+    eval_freq=EVAL_FREQ,
+    n_eval_episodes=N_EVAL_EPISODES,
     deterministic=True,
     render=False,
+    verbose=0,
+)
+ppo_eval_callback = EvalCallback(
+    ppo_eval_env,
+    log_path=PPO_EVAL_PATH,
+    eval_freq=EVAL_FREQ,
+    n_eval_episodes=N_EVAL_EPISODES,
+    deterministic=True,
+    render=False,
+    verbose=0,
 )
 
-grpo_model = GRPO("GroupPolicy", grpo_vec_env, verbose=1, group_size=16, learning_rate=0.001, kl_beta=0, group_rollout_buffer_class=StepwiseGroupBuffer)
+grpo_model = GRPO(
+    "GroupPolicy",
+    grpo_vec_env,
+    verbose=1,
+    group_size=16,
+    learning_rate=0.001,
+    kl_beta=0,
+    group_rollout_buffer_class=StepwiseGroupBuffer
+)
 # grpo_model = GRPO("GroupPolicy", grpo_vec_env, verbose=1, group_size=16, learning_rate=0.001, kl_beta=0)
-grpo_model.learn(total_timesteps=TRAINING_TIMESTEPS, callback=eval_callback)
-# model.save("grpo_cartpole")
+grpo_model.learn(total_timesteps=TRAINING_TIMESTEPS, callback=grpo_eval_callback)
 
-obs = grpo_vec_env.reset()
-episode_rewards = [[] for _ in range(grpo_vec_env.num_envs)]
-all_episode_rewards = []
+ppo_model = PPO("MlpPolicy", ppo_vec_env, verbose=1, learning_rate=0.001, batch_size=16)
+ppo_model.learn(total_timesteps=TRAINING_TIMESTEPS, callback=ppo_eval_callback)
 
-# GRPO eval
-for _ in range(EVAL_TIMESTEPS):
-    action, _ = grpo_model.predict(obs)
-    obs, rewards, dones, _ = grpo_vec_env.step(action)
-    # vec_env.render("human")
-
-    for i in range(grpo_vec_env.num_envs):
-        episode_rewards[i].append(rewards[i])
-        if dones[i]:
-            # Episode ended; store total reward
-            total_reward = sum(episode_rewards[i])
-            all_episode_rewards.append(total_reward)
-            episode_rewards[i] = []
-            # print(total_reward)
-
-if all_episode_rewards:
-    avg_reward = np.mean(all_episode_rewards)
-    print(f"GRPO: average reward per episode over {len(all_episode_rewards)} episodes: {avg_reward:.2f}")
-else:
-    print("No episodes finished during GRPO eval.")
-
-if USE_BASELINE:
-    # PPO eval as baseline
-    ppo_model = PPO("MlpPolicy", ppo_vec_env, verbose=0, learning_rate=0.001, batch_size=16)
-    ppo_model.learn(total_timesteps=TRAINING_TIMESTEPS)
-
+if RUN_GRPO:
     obs = grpo_vec_env.reset()
     episode_rewards = [[] for _ in range(grpo_vec_env.num_envs)]
     all_episode_rewards = []
 
-    for _ in range(EVAL_TIMESTEPS):
+    for _ in range(RUN_TIMESTEPS):
+        action, _ = grpo_model.predict(obs)
+        obs, rewards, dones, _ = grpo_vec_env.step(action)
+        # vec_env.render("human")
+
+        for i in range(grpo_vec_env.num_envs):
+            episode_rewards[i].append(rewards[i])
+            if dones[i]:
+                # Episode ended; store total reward
+                total_reward = sum(episode_rewards[i])
+                all_episode_rewards.append(total_reward)
+                episode_rewards[i] = []
+                # print(total_reward)
+
+    if all_episode_rewards:
+        avg_reward = np.mean(all_episode_rewards)
+        print(f"GRPO: average reward per episode over {len(all_episode_rewards)} episodes: {avg_reward:.2f}")
+    else:
+        print("No episodes finished during GRPO eval.")
+
+if RUN_PPO:
+    obs = grpo_vec_env.reset()
+    episode_rewards = [[] for _ in range(grpo_vec_env.num_envs)]
+    all_episode_rewards = []
+
+    for _ in range(RUN_TIMESTEPS):
         action, _ = ppo_model.predict(obs)
         obs, rewards, dones, _ = ppo_vec_env.step(action)
 
@@ -84,21 +109,29 @@ if USE_BASELINE:
     else:
         print("No episodes finished during PPO eval.")
 
-eval_path = "./eval_logs/evaluations.npz"
-data = np.load(eval_path)
-timesteps = data["timesteps"]
-results = data["results"]
+grpo_data = np.load(GRPO_EVAL_PATH + "evaluations.npz")
+grpo_timesteps = grpo_data["timesteps"]
+grpo_results = grpo_data["results"]
 
-mean_rewards = results.mean(axis=1)
-std_rewards = results.std(axis=1)
+grpo_mean_rewards = grpo_results.mean(axis=1)
+grpo_std_rewards = grpo_results.std(axis=1)
 
-plt.plot(timesteps, mean_rewards, label="Mean Evaluation Reward")
-plt.fill_between(timesteps, mean_rewards - std_rewards, mean_rewards + std_rewards, alpha=0.3)
+ppo_data = np.load(PPO_EVAL_PATH + "evaluations.npz")
+ppo_timesteps = ppo_data["timesteps"]
+ppo_results = ppo_data["results"]
+
+ppo_mean_rewards = ppo_results.mean(axis=1)
+ppo_std_rewards = ppo_results.std(axis=1)
+
+plt.plot(grpo_timesteps, grpo_mean_rewards, label="GRPO Mean Evaluation Reward")
+plt.fill_between(grpo_timesteps, grpo_mean_rewards - grpo_std_rewards, grpo_mean_rewards + grpo_std_rewards, alpha=0.3)
+plt.plot(ppo_timesteps, ppo_mean_rewards, label="PPO Mean Evaluation Reward")
+plt.fill_between(ppo_timesteps, ppo_mean_rewards - ppo_std_rewards, ppo_mean_rewards + ppo_std_rewards, alpha=0.3)
 plt.xlabel("Timesteps")
 plt.ylabel("Evaluation Reward")
 plt.title("Evaluation Performance Over Time")
 plt.legend()
 plt.grid()
 plt.tight_layout()
-plt.savefig("/eval_images/eval_performance.png")
+plt.savefig(f"./eval_images/eval_performance_{datetime.now()}.png")
 plt.show()
